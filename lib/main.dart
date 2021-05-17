9438c57c94402a0ff17b636d3b50fa65fd3ed952
danielmbrasil <danielmbrasil@protonmail.com>
2021-05-17 14:49:29 -0300

    check if token is still validate, if so redirect to homepage add bottom nav bar add loading screen, WaveLoading

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:oops/screens/authenticate/authenticate.dart';
+import 'package:http/http.dart' as http;
+import 'package:oops/screens/home/bottom_nav_bar.dart';
+import 'package:oops/screens/shared/wave_loading.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,8 +18,57 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Authenticate(),
+      home: CheckToken(),
     );
   }
 }
 
+class CheckToken extends StatefulWidget {
+  @override
+  _CheckTokenState createState() => _CheckTokenState();
+}
+
+class _CheckTokenState extends State<CheckToken> {
+  bool _isValid = false;
+  bool _loading = true;
+
+  @override
+  void initState() {
+    _isTokenValid();
+    super.initState();
+  }
+
+  void _isTokenValid() async {
+    try {
+      // get token and user_id
+      final storage = new FlutterSecureStorage();
+      String token = await storage.read(key: 'jwt');
+      String id = await storage.read(key: 'user_id');
+
+      final url = Uri.parse('http://10.0.2.2:3000/api/users/' + id);
+
+      final response = await http.get(url, headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': token
+      });
+
+      if (response.statusCode == 200) {
+        setState(() {
+          _isValid = true;
+          _loading = false;
+        });
+      } else {
+        setState(() => _loading = false);
+      }
+    } catch (_) {
+      setState(() => _loading = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _loading
+        ? WaveLoading()
+        : (_isValid ? BottomNavBar() : Authenticate());
+  }
+}
